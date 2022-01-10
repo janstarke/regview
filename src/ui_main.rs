3924b1c97c57684e463b039579ac9cce1d172214
Jan Starke <jan.starke@t-systems.com>
2022-01-10 17:34:37 +0100

    better layout

--- src/ui_main.rs
+++ src/ui_main.rs
@@ -1,9 +1,11 @@
 use anyhow::Result;
-use cursive::view::{Nameable, Resizable};
-use cursive::{views::{Dialog, TextView}, CursiveRunnable, theme::BaseColor};
+use cursive::view::{Nameable, Resizable, SizeConstraint};
+use cursive::{views::{Dialog, TextView, LinearLayout, ResizedView, Panel}, CursiveRunnable, theme::BaseColor};
 use cursive_table_view::{TableView, TableViewItem};
 use std::{cell::RefCell, borrow::BorrowMut};
 use cursive_aligned_view::Alignable;
+
+static PARENT_DIRECTORY: &str = "[..]";
 
 pub struct UIMain {
     siv: RefCell<CursiveRunnable>
@@ -38,7 +40,17 @@ impl TableViewItem<BasicColumn> for TableLine {
     where
             Self: Sized {
         match column {
-            BasicColumn::Name => self.name.cmp(&other.name)
+            BasicColumn::Name => {
+                if self.name == other.name {
+                    std::cmp::Ordering::Equal
+                } else if self.name == PARENT_DIRECTORY {
+                    std::cmp::Ordering::Less
+                } else if other.name == PARENT_DIRECTORY {
+                    std::cmp::Ordering::Greater
+                } else {
+                    self.name.cmp(&other.name)
+                }
+            }
         }
     }
 }
@@ -52,11 +64,30 @@ impl UIMain {
             .column(BasicColumn::Name, "Name", |c| c.width_percent(100));
         
         let mut items = vec![
-            TableLine::new("[..]"), 
+            TableLine::new(PARENT_DIRECTORY), 
             TableLine::new("Test1"),
             TableLine::new("Test2")];
         table.set_items(items);
-        siv.add_layer(Dialog::around(table.with_name("table").min_size((50, 20))).title("Table View"));
+
+        let mut details_table = TableView::<TableLine, BasicColumn>::new()
+            .column(BasicColumn::Name, "Name", |c| c.width_percent(100));
+
+        let mut items = vec![
+            TableLine::new("Details1"), 
+            TableLine::new("Details2"),
+            TableLine::new("Details3")];
+        details_table.set_items(items);
+        details_table.set_enabled(false);
+
+        let root_view = LinearLayout::horizontal()
+            .child(table.with_name("keys").full_screen())
+            .child(details_table.with_name("details").full_screen());
+
+        siv.add_layer(Panel::new(ResizedView::new(
+            SizeConstraint::Full,
+            SizeConstraint::Full,
+            root_view
+        )).title(format!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))));
 
         Self {
             siv: RefCell::new(siv)
