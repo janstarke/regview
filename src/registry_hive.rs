4552e425ed5b0f00d5f082d6a30d3c15827676a8
Jan Starke <jan.starke@t-systems.com>
2022-01-11 14:55:52 +0100

    path is being displayed

--- src/registry_hive.rs
+++ src/registry_hive.rs
@@ -8,11 +8,13 @@ use rwinreg::nk::NodeKey;
 use rwinreg::vk::Data;
 
 use crate::keys_line::KeysLine;
+use crate::values_line::ValuesLine;
 
 pub struct RegistryHive {
     hive_file: File,
     root: Rc<RefCell<NodeKey>>,
-    path: Vec<Rc<RefCell<NodeKey>>>
+    path: Vec<Rc<RefCell<NodeKey>>>,
+    visible_path: Vec<String>
 }
 
 impl RegistryHive {
@@ -22,14 +24,22 @@ impl RegistryHive {
         Ok(Self {
             hive_file: hive_file,
             root: Rc::new(RefCell::new(root)),
-            path: Vec::new()
+            path: Vec::new(),
+            visible_path: Vec::new()
         })
+    }
+
+    pub fn path(&self) -> String {
+        self.visible_path.join("\\")
     }
 
     pub fn current_keys(&mut self) -> Result<Vec<KeysLine>> {
         let mut keys = vec![KeysLine::parent()];
 
         loop {
+            //                                              question mark above sbwe's head ---------+
+            //                                                                                       |
+            //                                                                                       v
             let record = match self.root.borrow_mut().get_next_key(&mut self.hive_file)? {
                 None => { break; }
                 Some(node) => node
@@ -44,6 +54,7 @@ impl RegistryHive {
             None => (),
             Some(r) => {
                 self.root = r;
+                self.visible_path.pop();
             }
         };
         
@@ -51,8 +62,34 @@ impl RegistryHive {
     }
 
     pub fn child_keys(&mut self, item: Rc<RefCell<NodeKey>>) -> Result<Vec<KeysLine>> {
+        self.visible_path.push(self.root.borrow().key_name().to_owned());
         self.path.push(Rc::clone(&self.root));
         self.root = Rc::clone(&item);
         self.current_keys()
+    }
+
+
+    pub fn key_values(&mut self, record: Rc<RefCell<NodeKey>>) -> Result<Vec<ValuesLine>> {
+        let mut values = Vec::new();
+        loop {
+            let value = match record.borrow_mut().get_next_value(&mut self.hive_file)? {
+                None => {break;}
+                Some(node) => node
+            };
+
+            let data = match value.decode_data()? {
+                None => { "NONE".to_owned() }
+                Some(data) => {
+                    match data {
+                        Data::None => "NONE".to_owned(),
+                        Data::String(s) => s,
+                        Data::Int32(i) => i.to_string()
+
+                    }
+                }
+            };
+            values.push(ValuesLine::new(&data));
+        }
+        Ok(values)
     }
 }
--- src/ui_main.rs
+++ src/ui_main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use cursive::Cursive;
 use cursive::view::{Nameable, Resizable, SizeConstraint};
 use cursive::views::DummyView;
-use cursive::{views::{LinearLayout, ResizedView, Panel, NamedView, ViewRef}, CursiveRunnable};
+use cursive::{views::{LinearLayout, ResizedView, Panel, TextView, ViewRef}, CursiveRunnable};
 use cursive_table_view::{TableView};
 use cursive::event::{EventTrigger, EventResult};
 use std::{cell::RefCell};
@@ -13,6 +13,8 @@ use crate::values_line::*;
 
 static NAME_KEYS_PANE: &str = "keys_pane";
 static NAME_KEYS_TABLE: &str = "keys_table";
+static NAME_VALUES_TABLE: &str = "values_table";
+static NAME_PATH_LINE: &str = "path_line";
 
 pub struct UIMain {
     siv: RefCell<CursiveRunnable>
@@ -25,27 +27,25 @@ impl UIMain {
 
         let mut keys_table = TableView::<KeysLine, KeysColumn>::new()
             .column(KeysColumn::Name, "Name", |c| {c})
-            .column(KeysColumn::LastWritten, "Last written", |c| c.width(25))
-            ;
+            .column(KeysColumn::LastWritten, "Last written", |c| c.width(25));
 
         keys_table.set_on_submit(UIMain::on_submit);
-       
-        let table = NamedView::new(NAME_KEYS_TABLE, keys_table);
+        keys_table.set_on_select(UIMain::on_select);
 
         let mut details_table = TableView::<ValuesLine, ValuesColumn>::new()
             .column(ValuesColumn::Name, "Name", |c| c.width_percent(100));
 
-        let items = vec![
-            ValuesLine::new("Details1"), 
-            ValuesLine::new("Details2"),
-            ValuesLine::new("Details3")];
-        details_table.set_items(items);
         details_table.set_enabled(false);
 
-        let root_view = LinearLayout::horizontal()
-            .child(table.with_name("keys").full_screen())
+        let reg_view = LinearLayout::horizontal()
+            .child(keys_table.with_name(NAME_KEYS_TABLE).full_screen())
             .child(DummyView)
-            .child(details_table.with_name("details").full_screen());
+            .child(details_table.with_name(NAME_VALUES_TABLE).full_screen());
+
+        let root_view = LinearLayout::vertical()
+            .child(TextView::new("").with_name(NAME_PATH_LINE))
+            .child(DummyView)
+            .child(reg_view);
 
         siv.add_layer(Panel::new(ResizedView::new(
             SizeConstraint::Full,
@@ -61,10 +61,10 @@ impl UIMain {
 
     fn on_submit(siv: &mut Cursive, row: usize, index: usize) {
         let mut keys_table: ViewRef<TableView::<KeysLine, KeysColumn>> = siv.find_name(NAME_KEYS_TABLE).unwrap();
+        let hive: &mut RegistryHive = siv.user_data().unwrap();
         let new_items = match keys_table.borrow_item(index) {
             None => { return },
             Some(item) => {
-                let hive: &mut RegistryHive = siv.user_data().unwrap();
                 if item.is_parent() {
                     hive.parent_keys().unwrap()
                 } else {
@@ -74,6 +74,23 @@ impl UIMain {
         };
         keys_table.clear();
         keys_table.set_items(new_items);
+        let path = hive.path();
+        siv.call_on_name(NAME_PATH_LINE, |l: &mut TextView| l.set_content(path));
+    }
+
+    fn on_select(siv: &mut Cursive, row: usize, index: usize) {
+        let mut keys_table: ViewRef<TableView::<KeysLine, KeysColumn>> = siv.find_name(NAME_KEYS_TABLE).unwrap();
+        let new_items = match keys_table.borrow_item(index) {
+            None => { return },
+            Some(item) => {
+                let hive: &mut RegistryHive = siv.user_data().unwrap();
+                hive.key_values(item.record()).unwrap()
+            }
+        };
+
+        let mut values_table: ViewRef<TableView::<ValuesLine, ValuesColumn>> = siv.find_name(NAME_VALUES_TABLE).unwrap();
+        values_table.clear();
+        values_table.set_items(new_items);
     }
 
     pub fn run(&self) -> Result<()> {
